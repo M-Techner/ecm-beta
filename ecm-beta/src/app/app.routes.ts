fb2f6414f38486b4229d72b4d205aff33b92a696
Mags8934 <maggie.werning@gmail.com>
2024-04-08 05:34:41 -0500

    photos

--- ecm-beta/src/app/app.routes.ts
+++ ecm-beta/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BuyAHorseComponent } from './buy-a-horse/buy-a-horse.component';
+import { SellYourHorseComponent } from './sell-your-horse/sell-your-horse.component';
+import { OtherServicesComponent } from './other-services/other-services.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'buy-a-horse', component: BuyAHorseComponent },
+    { path: 'sell-your-horse', component: SellYourHorseComponent },
+    { path: 'other-services', component: OtherServicesComponent},
+    { path: 'contact', component: ContactComponent },
+    { path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
